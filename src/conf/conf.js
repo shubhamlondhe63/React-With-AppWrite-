9818ce99709449783d3e42f6d64e4b5ead269287
Shubham Londhe <shubhamlondhe63@gmail.com>
2024-03-21 18:36:21 +0530

    AppWrite configuration completed

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appWriteUrl: String(VITE_APP_WRITE_URL),
+  appWriteProjectId: String(VITE_APPWRITE_PROJECT_ID),
+  appDatabaseId: String(VITE_APPWRITE_DATABASE_ID),
+  appCollectionId: String(VITE_APPWRITE_COLLECTION_ID),
+  appBucketId: String(VITE_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
